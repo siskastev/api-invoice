bf900aa3f7bc19a60bf5ece0ecafcd2a1fb5e200
Siska Stevani <44636088+siskastev@users.noreply.github.com>
2023-07-08 22:16:35 +0700

    feat: get products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
@@ -13,9 +14,35 @@ class ProductController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $queryStringArray = $request->only('per_page', 'keyword');
+        $perPage = $queryStringArray['per_page'] ?? 10;
+        $keyword = $queryStringArray['keyword'] ?? null;
+
+        $currentPage = $request->query('page') ?: 1;
+
+        $query = Product::query();
+
+        if (!empty($keyword)) {
+            $query->where('name', 'like', '%' . $keyword . '%');
+            $query->orWhere('type', 'like', '%' . $keyword . '%');
+        }
+
+        $products = $query->where('deleted_at', null)->paginate($perPage);
+
+        return response()->json([
+            'messages' => "OK",
+            'data' => $products->items(),
+            'metadata' => [
+                'current_page' => $currentPage,
+                'from' => $products->firstItem(),
+                'last_page' => $products->lastPage(),
+                'per_page' => $products->perPage(),
+                'to' => $products->lastItem(),
+                'total' => $products->total(),
+            ],
+        ], 200);
     }
 
     /**
