0cc7cd43bb29e61a421879936b2de88e391de374
Siska Stevani <44636088+siskastev@users.noreply.github.com>
2023-07-08 20:37:42 +0700

    feat: add route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +17,21 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function() {
+        return auth()->user();
+    });
+    Route::get('/products', [ProductController::class, 'get']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    // Routes accessible to users with role 1 is admin
+    Route::group(['middleware' => ['role:1']], function () {
+        Route::get('/customers', [CustomerController::class, 'get']);
+        Route::post('/invoice', [InvoiceController::class, 'create']);
+        Route::get('/invoice/{code}', [InvoiceController::class, 'get']);
+    });
 });
