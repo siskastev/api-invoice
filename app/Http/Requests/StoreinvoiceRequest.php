4fd8e79daea3d38d01227b7cb35a5d209c33c631
Siska Stevani <44636088+siskastev@users.noreply.github.com>
2023-07-09 23:30:59 +0700

    feat: add rules for store invoice request validation

--- app/Http/Requests/StoreinvoiceRequest.php
+++ app/Http/Requests/StoreinvoiceRequest.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Requests;
 
+use App\Rules\ProductNameExist;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
-class StoreinvoiceRequest extends FormRequest
+class StoreInvoiceRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -13,7 +15,7 @@ class StoreinvoiceRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +26,15 @@ class StoreinvoiceRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'subject' => 'required',
+            'issue_date' => 'required|date_format:Y-m-d|after_or_equal:'.now()->format("Y-m-d"),
+            'due_date' => 'required|date_format:Y-m-d|after_or_equal:issue_date',
+            'customer_id' => ['required', Rule::exists('customers', 'id')->whereNull('deleted_at')],
+            'products' => 'required|array',
+            'products.*.product_id' => ['required', Rule::exists('products', 'id')->whereNull('deleted_at')],
+            'products.*.product_name' => ['required', new ProductNameExist],
+            'products.*.qty' => 'required|numeric|between:1,200',
+            'products.*.unit_price' => 'required|numeric|min:1',
         ];
     }
 }
