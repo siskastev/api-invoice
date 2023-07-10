6f8cfe85fb20609135d2a5b741f8e8a66b4074bd
Siska Stevani <44636088+siskastev@users.noreply.github.com>
2023-07-10 16:03:36 +0700

    feat: add API for CRUD customers and products

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\InvoiceController;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CustomerController;
+use App\Http\Controllers\Api\InvoiceController;
+use App\Http\Controllers\Api\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,20 +17,37 @@ use App\Http\Controllers\ProductController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 // Protected routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::get('/profile', function() {
+
+    Route::get('/profile', function () {
         return auth()->user();
     });
+
     Route::get('/products', [ProductController::class, 'index']);
+    Route::get('/products/{id}', [ProductController::class, 'show']);
+
     Route::post('/logout', [AuthController::class, 'logout']);
-    
+
     // Routes accessible to users with role 1 is admin
     Route::group(['middleware' => ['role:1']], function () {
+        //customers
+        Route::post('/customers', [CustomerController::class, 'store']);
+        Route::put('/customers/{id}', [CustomerController::class, 'update']);
         Route::get('/customers', [CustomerController::class, 'index']);
+        Route::get('/customers/{id}', [CustomerController::class, 'show']);
+        Route::delete('/customers/{id}', [CustomerController::class, 'destroy']);
+
+        //products
+        Route::post('/products', [ProductController::class, 'store']);
+        Route::put('/products/{id}', [ProductController::class, 'update']);
+        Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+        //invoice
         Route::get('/invoice', [InvoiceController::class, 'index']);
         Route::post('/invoice', [InvoiceController::class, 'store']);
         Route::put('/invoice/{code}', [InvoiceController::class, 'update']);
