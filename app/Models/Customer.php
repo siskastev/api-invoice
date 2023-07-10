19bea0202154bca32d364cffa1b162409d680e1f
Siska Stevani <44636088+siskastev@users.noreply.github.com>
2023-07-10 20:34:46 +0700

    feat: add softDeletes customers

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -5,12 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\UUID;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Customer extends Model
 {
-    use HasFactory, UUID;
+    use HasFactory, UUID, SoftDeletes;
 
     protected $fillable = [
         'name', 'country', 'city', 'address'
     ];
+
 }
